74b163ae629eefe77a4ad8177064bd71bbf97118
hzheng522 <146857909+hzheng522@users.noreply.github.com>
2025-04-14 23:50:23 +0000

    debug

--- astrology_site/astrology_site/settings.py
+++ astrology_site/astrology_site/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -124,4 +124,4 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 import os
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'astrology/static')]
+STATICFILES_DIRS = [BASE_DIR / "static"]
