ede2fd7e7f5ab27e7423f750f1e714e1a66f4815
hzheng522 <146857909+hzheng522@users.noreply.github.com>
2025-04-17 20:00:52 +0000

    working now

--- astrology_site/astrology_site/urls.py
+++ astrology_site/astrology_site/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('house/', views.house, name='house'),
     path('planet/', views.planet, name='planet'),
     path('chart/', views.birth_chart_view, name='chart'),
+    path('generate_chart/', views.generate_chart, name='generate_chart'),
 ]
 
 
